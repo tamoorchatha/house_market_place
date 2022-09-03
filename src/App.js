726f46bf444ade45d4f4e3de2fc2ec39bcf44749
tamoorchatha <tamoorchatha82@gmail.com>
2022-09-04 02:04:48 +0500

    routes added and navbar also added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Explore from "./pages/Explore";
+import Forgot from "./pages/Forgot";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Explore />} />
+            <Route path="/offers" element={<Offers />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forgot-password" element={<Forgot />} />
+        </Routes>
+        <Navbar />
+      </Router>
+
     </div>
   );
 }
